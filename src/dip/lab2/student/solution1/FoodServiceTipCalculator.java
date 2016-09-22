af982f13b320d948858e01ef68dafe23ba773f21
Carson Schultz <Carson Schultz@Icecscomputer.attlocal.net>
2016-09-22 04:13:31 -0500

    firstCommit

---
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -0,0 +1,81 @@
+package dip.lab2.student.solution1;
+
+public class FoodServiceTipCalculator implements TipService{
+    private static final double MIN_BILL = 0.00;
+    private static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
+    private double goodRate = 0.20;
+    private double fairRate = 0.15;
+    private double poorRate = 0.10;
+
+    private double bill;
+    public enum ServiceQuality {
+        GOOD, FAIR, POOR
+    }
+    private ServiceQuality serviceQuality;
+
+    public FoodServiceTipCalculator(ServiceQuality q, double billAmt) {
+        this.setServiceRating(q);
+        this.setBill(billAmt);
+    }
+
+    @Override
+    public double getTip() {
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = bill * goodRate;
+                break;
+            case FAIR:
+                tip = bill * fairRate;
+                break;
+            case POOR:
+                tip = bill * poorRate;
+                break;
+        }
+
+        return tip;
+    }
+
+    public final void setBill(double billAmt) {
+        if(billAmt < MIN_BILL) {
+            throw new IllegalArgumentException(BILL_ENTRY_ERR);
+        }
+        bill = billAmt;
+    }
+
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public double getGoodRate() {
+        return goodRate;
+    }
+
+    public void setGoodRate(double goodRate) {
+        this.goodRate = goodRate;
+    }
+
+    public double getFairRate() {
+        return fairRate;
+    }
+
+    public void setFairRate(double fairRate) {
+        this.fairRate = fairRate;
+    }
+
+    public double getPoorRate() {
+        return poorRate;
+    }
+
+    public void setPoorRate(double poorRate) {
+        this.poorRate = poorRate;
+    }
+
+}
